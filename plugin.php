3036d5db2a95a77c74d5df28d6b5f0512d62b794
Артем <artikus.sol@gmail.com>
2022-10-11 00:02:08 +0400

    VER - смена версии, CHANGELOG.md, README.md

--- plugin.php
+++ plugin.php
@@ -12,7 +12,7 @@ namespace Ainsys\Connector\Master;
  * Plugin Name:       AINSYS connector
  * Plugin URI: https://app.ainsys.com/
  * Description: AINSYS connector master.
- * Version:           4.0.0
+ * Version:           4.1.0
  * Author:            AINSYS
  * Author URI:        https://app.ainsys.com/
  * License: GPLv2 or later

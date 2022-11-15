a13adf8d1fdf71c648a1382416ecc69e225b038d
Артем <artikus.sol@gmail.com>
2022-11-16 03:52:28 +0400

    UPD - правки и рефакторинг при отправке данных юзера

--- includes/wp/class-process-users.php
+++ includes/wp/class-process-users.php
@@ -2,7 +2,6 @@
 
 namespace Ainsys\Connector\Master\WP;
 
-
 use Ainsys\Connector\Master\Core;
 use Ainsys\Connector\Master\Hooked;
 use Ainsys\Connector\Master\Logger;
@@ -12,14 +11,16 @@ class Process_Users implements Hooked {
 	/**
 	 * @var Core
 	 */
-	private $core;
+	private Core $core;
 
 	/**
 	 * @var Logger
 	 */
-	private $logger;
+	private Logger $logger;
+
 
 	public function __construct( Core $core, Logger $logger ) {
+
 		$this->core   = $core;
 		$this->logger = $logger;
 	}
@@ -31,55 +32,44 @@ class Process_Users implements Hooked {
 	 * @return void
 	 */
 	public function init_hooks() {
-		add_action( 'user_register', array( $this, 'process_new_user' ), 10, 2 );
-		add_action( 'profile_update', array( $this, 'send_user_details_update_to_ainsys' ), 10, 4 );
+
+		add_action( 'user_register', [ $this, 'process_new_user' ], 10, 2 );
+		add_action( 'profile_update', [ $this, 'send_user_details_update_to_ainsys' ], 10, 4 );
 	}
+
 
 	/**
 	 * Sends new user details to AINSYS
 	 *
-	 * @param int $user_id
-	 * @param array $userdata
+	 * @param  int   $user_id
+	 * @param  array $userdata
 	 *
 	 * @return void
 	 */
-	public function process_new_user( $user_id, $userdata ) {
+	public function process_new_user( int $user_id, array $userdata ): void {
+
 		$request_action = 'CREATE';
 
 		$fields = apply_filters( 'ainsys_new_user_fields', $this->prepare_user_data( $user_id, $userdata ), $userdata );
 
-		$request_data = array(
-			'entity'  => array(
-				'id'   => $user_id,
-				'name' => 'user',
-			),
-			'action'  => $request_action,
-			'payload' => $fields,
-		);
+		$this->send_data( $user_id, $request_action, $fields );
 
-		try {
-			$server_response = $this->core->curl_exec_func( $request_data );
-		} catch ( \Exception $e ) {
-			$server_response = 'Error: ' . $e->getMessage();
-		}
-
-		$this->logger::save_log_information( $user_id, $request_action, serialize( $request_data ), serialize( $server_response ), 0 );
-
-		return;
 	}
+
 
 	/**
 	 * Prepares WP user data. Adds ACF fields if there are any.
 	 *
-	 * @param int $user_id
-	 * @param array $data
+	 * @param  int   $user_id
+	 * @param  array $data
 	 *
 	 * @return array
 	 */
 	private function prepare_user_data( $user_id, $data ) {
+
 		//$data['id'] = $user_id;
 		/// Get ACF fields
-		$acf_fields = apply_filters( 'ainsys_prepare_extra_user_data', array(), $user_id );
+		$acf_fields = apply_filters( 'ainsys_prepare_extra_user_data', [], $user_id );
 
 		return array_merge( $data, $acf_fields );
 	}
@@ -88,25 +78,46 @@ class Process_Users implements Hooked {
 	/**
 	 * Sends updated user details to AINSYS.
 	 *
-	 * @param int $user_id
-	 * @param array $old_user_data
-	 * @param array $userdata
+	 * @param  int   $user_id
+	 * @param  array $old_user_data
+	 * @param  array $userdata
+	 * @param  bool  $test
 	 *
-	 * @return
+	 * @return array|void
+	 * @reference in multisite mode, users are created without a password,
+	 * a password is created automatically or when clicking on a link, because this hook triggers the user creation field
 	 */
 	public function send_user_details_update_to_ainsys( $user_id, $old_user_data, $userdata, $test = false ) {
+
 		$request_action = 'UPDATE';
 
 		$fields = apply_filters( 'ainsys_user_details_update_fields', $this->prepare_user_data( $user_id, $userdata ), $userdata );
 
-		$request_data = array(
-			'entity'  => array(
+		$request_test = $this->send_data( $user_id, $request_action, $fields );
+
+		if ( $test ) {
+			return $request_test;
+		}
+	}
+
+
+	/**
+	 * @param  int    $user_id
+	 * @param  string $request_action
+	 * @param         $fields
+	 *
+	 * @return array
+	 */
+	protected function send_data( int $user_id, string $request_action, $fields ): array {
+
+		$request_data = [
+			'entity'  => [
 				'id'   => $user_id,
 				'name' => 'user',
-			),
+			],
 			'action'  => $request_action,
 			'payload' => $fields,
-		);
+		];
 
 		try {
 			$server_response = $this->core->curl_exec_func( $request_data );
@@ -115,17 +126,12 @@ class Process_Users implements Hooked {
 			$this->core->send_error_email( $server_response );
 		}
 
-		$this->logger->save_log_information( $user_id, $request_action, serialize( $request_data ), serialize( $server_response ), 0 );
+		$this->logger::save_log_information( $user_id, $request_action, serialize( $request_data ), serialize( $server_response ), 0 );
 
-		if ( $test ) {
-			$result = array(
-				'request'  => $request_data,
-				'response' => $server_response,
-			);
-			return $result;
-		} else {
-			return;
-		}
+		return [
+			'request'  => $request_data,
+			'response' => $server_response,
+		];
 	}
 
 }

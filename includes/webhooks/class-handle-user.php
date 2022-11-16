58f2309d13b4309d04be7506bd706f5f514a0bad
Артем <artikus.sol@gmail.com>
2022-11-16 04:41:34 +0400

    UPD - правки и рефакторинг класса для получения данных юзера на сайте

--- includes/webhooks/class-handle-user.php
+++ includes/webhooks/class-handle-user.php
@@ -2,13 +2,30 @@
 
 namespace Ainsys\Connector\Master\Webhooks;
 
+use Ainsys\Connector\Master\Core;
 use Ainsys\Connector\Master\Hooked;
+use Ainsys\Connector\Master\Logger;
 use Ainsys\Connector\Master\Webhook_Handler;
 
 class Handle_User implements Hooked, Webhook_Handler {
 
-	public function __construct() {
+	/**
+	 * @var \Ainsys\Connector\Master\Logger
+	 */
+	protected Logger $logger;
+
+	/**
+	 * @var Core
+	 */
+	private Core $core;
+
+
+	public function __construct( Core $core, Logger $logger ) {
+
+		$this->core   = $core;
+		$this->logger = $logger;
 	}
+
 
 	/**
 	 * Initializes WordPress hooks for component.
@@ -16,41 +33,100 @@ class Handle_User implements Hooked, Webhook_Handler {
 	 * @return void
 	 */
 	public function init_hooks() {
-		add_filter( 'ainsys_webhook_action_handlers', array( $this, 'register_webhook_handler' ), 10, 1 );
+
+		add_filter( 'ainsys_webhook_action_handlers', [ $this, 'register_webhook_handler' ], 10, 1 );
 	}
 
-	public function register_webhook_handler( $handlers = array() ) {
-		$handlers['user'] = array( $this, 'handler' );
+
+	public function register_webhook_handler( $handlers = [] ) {
+
+		$handlers['user'] = [ $this, 'handler' ];
 
 		return $handlers;
 	}
 
 
 	public function handler( $action, $data, $object_id = 0 ) {
-		$data              = (array) $data;
-		$data['user_pass'] = $data['user_pass'] ?? wp_generate_password( 15, true, true );
+
+		$data = (array) $data;
+
+		$response = __( 'Action not registered', AINSYS_CONNECTOR_TEXTDOMAIN );
 
 		switch ( $action ) {
-			case 'add':
-				$user_id = wp_insert_user( $data );
-				if ( ! is_wp_error( $user_id ) ) {
-					return $user_id;
-				} else {
-					return $user_id->get_error_message();
-				}
-			case 'update':
-				return $this->update_user( $data, $object_id );
-			case 'delete':
-				return wp_delete_user( $object_id );
+			case 'CREATE':
+				$response = $this->create_user( $data );
+				break;
+			case 'UPDATE':
+				$response = $this->update_user( $data, $object_id );
+				break;
+			case 'DELETE':
+				$response = wp_delete_user( $object_id );
+				break;
 		}
 
-		return 'Action not registered';
+		return $response;
 	}
 
-	private function update_user( $data, $object_id = 0 ) {
+
+	private function update_user( $data ): string {
+
 		$result = wp_update_user( $data );
 
-		return is_wp_error( $result ) ? $result->get_error_message() : $result;
+		$success = __( 'The user has been successfully updated: user ID ', AINSYS_CONNECTOR_TEXTDOMAIN );
+		$error   = __( 'An error has occurred, perhaps such a user does not exist', AINSYS_CONNECTOR_TEXTDOMAIN );
+
+		$message = $success . $result;
+
+		if ( is_wp_error( $result ) ) {
+			$message = $error . $result->get_error_message();
+			$this->core->send_error_email( $message );
+
+			return $message;
+		}
+
+		$this->logger::save_log_information(
+			0,
+			'UPDATE',
+			serialize( $data ),
+			$message
+		);
+
+		return $message;
+	}
+
+
+	/**
+	 * @param  array $data
+	 *
+	 * @return string
+	 */
+	protected function create_user( array $data ): string {
+
+		$success = __( 'The user has been successfully created: user ID ', AINSYS_CONNECTOR_TEXTDOMAIN );
+		$error   = __( 'An error occurred when creating a user: ', AINSYS_CONNECTOR_TEXTDOMAIN );
+
+		$data['user_pass'] = $data['user_pass'] ?? wp_generate_password( 15, true, true );
+
+		$user_id = wp_insert_user( $data );
+
+		$message = $success . $user_id;
+
+		if ( is_wp_error( $user_id ) ) {
+			$message = $error . $user_id->get_error_message();
+			$this->core->send_error_email( $message );
+
+			return $message;
+		}
+
+		$this->logger::save_log_information(
+			0,
+			'CREATE',
+			serialize( $data ),
+			$message
+		);
+
+		return $message;
+
 	}
 
 }

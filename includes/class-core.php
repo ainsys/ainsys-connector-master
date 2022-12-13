915113d60adb7834e53779994ab01c17fd5c682e
Артем <artikus.sol@gmail.com>
2022-12-13 22:52:21 +0400

    RFG - удаление конструктора, использование статических методов

--- includes/class-core.php
+++ includes/class-core.php
@@ -14,28 +14,11 @@ use Ainsys\Connector\Master\Settings\Settings;
 class Core implements Hooked {
 
 	/**
-	 * @var Logger
-	 */
-	private $logger;
-
-	/**
-	 * @var Settings
-	 */
-	private $settings;
-
-	public function __construct( Logger $logger, Settings $settings ) {
-		$this->logger   = $logger;
-		$this->settings = $settings;
-
-	}
-
-	/**
 	 * Hooks init to WP.
 	 *
 	 */
-	public function init_hooks() {
+	public function init_hooks() { }
 
-	}
 
 	/**
 	 * Curl connect and get data.
@@ -45,12 +28,13 @@ class Core implements Hooked {
 	 *
 	 * @return string
 	 */
-	public function curl_exec_func( array $post_fields = [], string $url = '' ): string {
-		$url = $url ? : (string) $this->settings::get_option( 'ansys_api_key' );
+	public static function curl_exec_func( array $post_fields = [], string $url = '' ): string {
+
+		$url = $url ? : (string) Settings::get_option( 'ansys_api_key' );
 
 		if ( empty( $url ) ) {
 
-			$this->logger::save_log_information(
+			Logger::save_log_information(
 				[
 					'object_id'       => 0,
 					'entity'          => 'cURL',
@@ -66,21 +50,21 @@ class Core implements Hooked {
 
 		$response = wp_remote_post(
 			$url,
-			array(
+			[
 				'timeout'     => 30,
 				'redirection' => 10,
 				'httpversion' => '1.0',
 				'blocking'    => true,
-				'headers'     => array( 'content-type' => 'application/json' ),
+				'headers'     => [ 'content-type' => 'application/json' ],
 				'body'        => wp_json_encode( $post_fields, 256 ),
-				'cookies'     => array(),
+				'cookies'     => [],
 				'sslverify'   => false,
-			)
+			]
 		);
 
 		if ( is_wp_error( $response ) ) {
 
-			$this->logger::save_log_information(
+			Logger::save_log_information(
 				[
 					'object_id'       => 0,
 					'entity'          => 'cURL',
@@ -96,19 +80,21 @@ class Core implements Hooked {
 		return $response['body'] ?? '';
 	}
 
+
 	/**
 	 * Send email in case of AINSYS server errors.
 	 *
-	 * @param string $message
+	 * @param  string $message
 	 */
-	public function send_error_email( $message ) {
+	public static function send_error_email( $message ) {
+
 		$mail_to = '';
-		if ( ! empty( $this->settings::get_backup_email() ) && filter_var( $this->settings::get_backup_email(), FILTER_VALIDATE_EMAIL ) ) {
-			$mail_to .= $this->settings::get_backup_email();
+		if ( ! empty( Settings::get_backup_email() ) && filter_var( Settings::get_backup_email(), FILTER_VALIDATE_EMAIL ) ) {
+			$mail_to .= Settings::get_backup_email();
 		}
-		for ( $i = 1; $i < 10; $i++ ) {
-			if ( ! empty( $this->settings::get_backup_email( $i ) ) && filter_var( $this->settings::get_backup_email( $i ), FILTER_VALIDATE_EMAIL ) ) {
-				$mail_to .= ',' . $this->settings::get_backup_email( $i );
+		for ( $i = 1; $i < 10; $i ++ ) {
+			if ( ! empty( Settings::get_backup_email( $i ) ) && filter_var( Settings::get_backup_email( $i ), FILTER_VALIDATE_EMAIL ) ) {
+				$mail_to .= ',' . Settings::get_backup_email( $i );
 			}
 		}
 

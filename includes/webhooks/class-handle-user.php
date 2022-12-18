3100539280dd9ac4c980da814fa5abdbde1cfcf8
Артем <artikus.sol@gmail.com>
2022-12-19 03:25:58 +0400

    RFG - вынос методов в отдельный класс

--- includes/webhooks/class-handle-user.php
+++ includes/webhooks/class-handle-user.php
@@ -3,12 +3,11 @@
 namespace Ainsys\Connector\Master\Webhooks;
 
 use Ainsys\Connector\Master\Conditions;
-use Ainsys\Connector\Master\Core;
 use Ainsys\Connector\Master\Hooked;
 use Ainsys\Connector\Master\Logger;
 use Ainsys\Connector\Master\Webhook_Handler;
 
-class Handle_User implements Hooked, Webhook_Handler {
+class Handle_User extends Handle implements Hooked, Webhook_Handler {
 
 	protected static string $entity = 'user';
 
@@ -152,46 +151,6 @@ class Handle_User implements Hooked, Webhook_Handler {
 		);
 
 		return $message;
-	}
-
-
-	private function handle_error( $data, $result, $message_error, $entity, $action ): string {
-
-		$message = $message_error . $result->get_error_message();
-
-		Logger::save(
-			[
-				'object_id'       => 0,
-				'entity'          => $entity,
-				'request_action'  => $action,
-				'request_type'    => 'incoming',
-				'request_data'    => serialize( $data ),
-				'server_response' => serialize( $message ),
-				'error'           => 1,
-			]
-		);
-
-		Core::send_error_email( $message );
-
-		return $message;
-	}
-
-
-	/**
-	 * @param $action
-	 * @param $user_id
-	 *
-	 * @return string
-	 */
-	private function message_success( $action, $user_id ): string {
-
-		return sprintf(
-			__( '%s has been successfully %s - %s ID:  %s', AINSYS_CONNECTOR_TEXTDOMAIN ),
-			ucwords( strtolower( self::$entity ) ),
-			strtolower( $action ),
-			$user_id,
-			self::$entity
-		);
 	}
 
 }

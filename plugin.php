deb4f98fea3565d09f7063e674b8efe5abb4c243
art-ainsys <112867520+art-ainsys@users.noreply.github.com>
2023-01-16 21:52:48 +0400

    GHA - настройка деплоя (#51)
    Co-authored-by: Артем <artikus.sol@gmail.com>

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ namespace Ainsys\Connector\Master;
  * Plugin Name:       AINSYS Integrations framework connector
  * Plugin URI: https://app.ainsys.com/
  * Description: AINSYS connector master.
- * Version:           4.3.6.1
+ * Version:           4.3.7
  * Author:            AINSYS
  * Author URI:        https://ainsys.com/
  * License: GPLv2 or later

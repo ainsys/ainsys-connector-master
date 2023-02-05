206c0c70d538eeeabf74dbf22a73ccfa08078c76
Артем <artikus.sol@gmail.com>
2023-02-06 02:31:08 +0400

    UPD - исправление обрабобтки передачи записей и страниц

--- includes/wp/class-process-posts.php
+++ includes/wp/class-process-posts.php
@@ -4,6 +4,7 @@ namespace Ainsys\Connector\Master\WP;
 
 use Ainsys\Connector\Master\Conditions;
 use Ainsys\Connector\Master\Hooked;
+use WP_Post;
 
 class Process_Posts extends Process implements Hooked {
 
@@ -17,24 +18,26 @@ class Process_Posts extends Process implements Hooked {
 	 */
 	public function init_hooks() {
 
-		add_action( 'wp_after_insert_post', [ $this, 'process_create' ], 10, 4 );
-		add_action( 'save_post_post', [ $this, 'process_update' ], 10, 4 );
+		add_action( 'wp_after_insert_post', [ $this, 'process_create' ], 1000, 4 );
+		add_action( 'wp_after_insert_post', [ $this, 'process_update' ], 1010, 4 );
 		add_action( 'deleted_post', [ $this, 'process_delete' ], 10, 2 );
 
 	}
 
 
 	/**
-	 * Sends new attachment details to AINSYS
+	 * Sends new post details to AINSYS
 	 *
-	 * @param  int $post_id
-	 * @param      $post
-	 * @param      $update
-	 * @param      $post_before
+	 * @param  int          $post_id     Post ID.
+	 * @param  WP_Post      $post        Post object.
+	 * @param  bool         $update      Whether this is an existing post being updated.
+	 * @param  WP_Post|null $post_before Null for new posts, the WP_Post object prior
+	 *                                   to the update for updated posts.
 	 *
 	 * @return void
 	 */
-	public function process_create( int $post_id, $post, $update, $post_before ): void {
+	public function process_create( int $post_id, WP_Post $post, bool $update, ?WP_Post $post_before ): void {
+
 
 		self::$action = 'CREATE';
 
@@ -42,7 +45,11 @@ class Process_Posts extends Process implements Hooked {
 			return;
 		}
 
-		if ( ! is_null( $post_before ) || true === $update ) {
+		if ( 'auto-draft' === $post->post_status ) {
+			return;
+		}
+
+		if ( $post_before && 'publish' === $post_before->post_status ) {
 			return;
 		}
 
@@ -64,11 +71,14 @@ class Process_Posts extends Process implements Hooked {
 	/**
 	 * Sends updated post details to AINSYS.
 	 *
-	 * @param       $post_id
-	 * @param       $post
-	 * @param       $update
+	 * @param  int          $post_id     Post ID.
+	 * @param  WP_Post      $post        Post object.
+	 * @param  bool         $update      Whether this is an existing post being updated.
+	 * @param  WP_Post|null $post_before Null for new posts, the WP_Post object prior
+	 *                                   to the update for updated posts.
 	 */
-	public function process_update( $post_id, $post, $update ): void {
+	public function process_update( int $post_id, WP_Post $post, bool $update, ?WP_Post $post_before ): void {
+
 
 		self::$action = 'UPDATE';
 
@@ -76,7 +86,15 @@ class Process_Posts extends Process implements Hooked {
 			return;
 		}
 
-		if ( ! $this->is_updated( $post_id, $update ) ) {
+		if ( 'auto-draft' === $post->post_status ) {
+			return;
+		}
+
+		if ( ( $post && $post_before ) && ( $post->post_modified > $post_before->post_modified ) ) {
+			return;
+		}
+
+		if ( ! $this->is_updated( $post_id, $post, $update ) ) {
 			return;
 		}
 
@@ -97,12 +115,12 @@ class Process_Posts extends Process implements Hooked {
 	/**
 	 * Sends delete post details to AINSYS
 	 *
-	 * @param  int $post_id
-	 * @param      $post
+	 * @param  int     $post_id
+	 * @param  WP_Post $post Post object.
 	 *
 	 * @return void
 	 */
-	public function process_delete( int $post_id, $post ): void {
+	public function process_delete( int $post_id, WP_Post $post ): void {
 
 		self::$action = 'DELETE';
 
@@ -122,14 +140,15 @@ class Process_Posts extends Process implements Hooked {
 
 
 	/**
+	 * Sends checking post details to AINSYS.
 	 *
-	 * @param       $post_id
-	 * @param       $post
-	 * @param       $update
+	 * @param  int     $post_id Post ID.
+	 * @param  WP_Post $post    Post object.
+	 * @param  bool    $update  Whether this is an existing post being updated.
 	 *
 	 * @return array
 	 */
-	public function process_checking( $post_id, $post, $update ): array {
+	public function process_checking( int $post_id, WP_Post $post, bool $update ): array {
 
 		self::$action = 'CHECKING';
 
@@ -137,7 +156,7 @@ class Process_Posts extends Process implements Hooked {
 			return [];
 		}
 
-		if ( ! $this->is_updated( $post_id, $update ) ) {
+		if ( ! $this->is_updated( $post_id, $post, $update ) ) {
 			return [];
 		}
 
@@ -156,42 +175,41 @@ class Process_Posts extends Process implements Hooked {
 
 
 	/**
+	 * Function for `add_attachment` action-hook.
 	 *
-	 * @param  int $post_ID Post ID.
-	 * @param      $post
+	 * @param  int     $post_ID Post ID.
+	 * @param  WP_Post $post    Post object.
 	 *
 	 * @return array
 	 */
-	protected function prepare_data( int $post_ID, $post ): array {
-
-		if ( ! $post ) {
-			$post = get_post( $post_ID );
-		}
+	protected function prepare_data( int $post_ID, WP_Post $post ): array {
 
 		if ( $post->post_type !== self::$entity ) {
 			return [];
 		}
 
-		return [
-			'ID'             => $post->ID,
-			'post_title'     => $post->post_title,
-			'post_content'   => $post->post_content,
-			'post_excerpt'   => $post->post_excerpt,
-			'post_status'    => $post->post_status,
-			'post_type'      => $post->post_type,
-			'post_author'    => $post->post_author,
-			'post_date'      => $post->post_date,
-			'post_modified'  => $post->post_modified,
-			'post_password'  => $post->post_password,
-			'pinged'         => $post->pinged,
-			'post_parent'    => 0,
-			'menu_order'     => $post->menu_order,
-			'guid'           => $post->guid,
-			'comment_status' => $post->comment_status,
-			'comment_count'  => $post->comment_count,
-			'taxonomies'     => $this->get_taxonomies( $post ),
-			'meta_input'     => get_post_meta( $post->ID ),
-		];
+		return array_merge(
+			[
+				'post_id'        => $post->ID,
+				'post_title'     => $post->post_title,
+				'post_content'   => $post->post_content,
+				'post_excerpt'   => $post->post_excerpt,
+				'post_author'    => (int) $post->post_author,
+				'post_status'    => $post->post_status,
+				'post_type'      => $post->post_type,
+				'post_date'      => $post->post_date,
+				'post_modified'  => $post->post_modified,
+				'post_password'  => $post->post_password,
+				'post_parent'    => $post->post_parent,
+				'menu_order'     => $post->menu_order,
+				'post_slug'      => $post->post_name,
+				'post_link'      => $post->guid,
+				'comment_status' => $post->comment_status,
+				'comment_count'  => (int) $post->comment_count,
+				'custom_fields'  => get_post_meta( $post->ID ),
+			],
+			$this->get_taxonomies( $post )
+		);
 	}
 
 }

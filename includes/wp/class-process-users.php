81bedaee802007706665fd195c7b35512edc9167
Артем <artikus.sol@gmail.com>
2023-02-09 15:02:44 +0400

    UPD - добавлены все поля юзера и их значение

--- includes/wp/class-process-users.php
+++ includes/wp/class-process-users.php
@@ -18,20 +18,19 @@ class Process_Users extends Process implements Hooked {
 	 */
 	public function init_hooks() {
 
-		add_action( 'user_register', [ $this, 'process_create' ], 10, 2 );
-		add_action( 'profile_update', [ $this, 'process_update' ], 10, 4 );
+		add_action( 'user_register', [ $this, 'process_create' ], 10, 1 );
+		add_action( 'profile_update', [ $this, 'process_update' ], 10, 1 );
 	}
 
 
 	/**
 	 * Sends new user details to AINSYS
 	 *
-	 * @param  int   $user_id
-	 * @param  array $userdata
+	 * @param  int $user_id
 	 *
 	 * @return void
 	 */
-	public function process_create( int $user_id, array $userdata ): void {
+	public function process_create( int $user_id ): void {
 
 		self::$action = 'CREATE';
 
@@ -41,8 +40,8 @@ class Process_Users extends Process implements Hooked {
 
 		$fields = apply_filters(
 			'ainsys_process_create_fields_' . self::$entity,
-			$this->prepare_data( $user_id, $userdata ),
-			$userdata
+			$this->prepare_data( $user_id ),
+			$user_id
 		);
 
 		$this->send_data( $user_id, self::$entity, self::$action, $fields );
@@ -53,16 +52,13 @@ class Process_Users extends Process implements Hooked {
 	/**
 	 * Sends updated user details to AINSYS.
 	 *
-	 * @param  int   $user_id
-	 * @param  array $userdata
-	 *
-	 * @param  array $old_user_data
+	 * @param  int $user_id
 	 *
 	 * @return void
 	 * @reference in multisite mode, users are created without a password,
 	 * a password is created automatically or when clicking on a link, because this hook triggers the user creation field
 	 */
-	public function process_update( $user_id, $userdata, $old_user_data ): void {
+	public function process_update( int $user_id ): void {
 
 		self::$action = 'UPDATE';
 
@@ -83,14 +79,11 @@ class Process_Users extends Process implements Hooked {
 
 	/**
 	 *
-	 * @param  int   $user_id
-	 * @param  array $userdata
-	 *
-	 * @param  array $old_user_data
+	 * @param  int $user_id
 	 *
 	 * @return array
 	 */
-	public function process_checking( $user_id, $userdata, $old_user_data ): array {
+	public function process_checking( int $user_id ): array {
 
 		self::$action = 'CHECKING';
 
@@ -126,43 +119,46 @@ class Process_Users extends Process implements Hooked {
 			'user_pass'                => $user->get( 'user_pass' ),
 			'user_email'               => $user->get( 'user_email' ),
 			'user_nicename'            => $user->get( 'user_nicename' ),
+			'user_nickname'            => $user->get( 'nickname' ),
 			'user_display_name'        => $user->get( 'display_name' ),
 			'user_registered'          => $user->get( 'user_registered' ),
 			'user_first_name'          => $user->get( 'first_name' ),
 			'user_last_name'           => $user->get( 'last_name' ),
 			'user_description'         => $user->get( 'description' ),
 			'user_link_website'        => $user->get( 'user_url' ),
-			'user_link_facebook'       => $user->get( 'facebook' ),
-			'user_link_instagram'      => $user->get( 'instagram' ),
-			'user_link_linkedin'       => $user->get( 'linkedin' ),
-			'user_link_myspace'        => $user->get( 'myspace' ),
-			'user_link_pinterest'      => $user->get( 'pinterest' ),
-			'user_link_youtube'        => $user->get( 'youtube' ),
-			'user_link_twitter'        => $user->get( 'twitter' ),
-			'user_link_tumblr'         => $user->get( 'tumblr' ),
-			'user_link_soundcloud'     => $user->get( 'soundcloud' ),
-			'user_billing_first_name'  => $user->get( 'billing_first_name' ),
-			'user_billing_last_name'   => $user->get( 'billing_last_name' ),
-			'user_billing_company'     => $user->get( 'billing_company' ),
-			'user_billing_address_1'   => $user->get( 'billing_address_1' ),
-			'user_billing_address_2'   => $user->get( 'billing_address_2' ),
-			'user_billing_city'        => $user->get( 'billing_city' ),
-			'user_billing_postcode'    => $user->get( 'billing_postcode' ),
-			'user_billing_country'     => $user->get( 'billing_country' ),
-			'user_billing_state'       => $user->get( 'billing_state' ),
-			'user_billing_phone'       => $user->get( 'billing_phone' ),
-			'user_billing_email'       => $user->get( 'billing_email' ),
-			'user_shipping_first_name' => $user->get( 'shipping_first_name' ),
-			'user_shipping_last_name'  => $user->get( 'shipping_last_name' ),
-			'user_shipping_company'    => $user->get( 'shipping_company' ),
-			'user_shipping_address_1'  => $user->get( 'shipping_address_1' ),
-			'user_shipping_address_2'  => $user->get( 'shipping_address_2' ),
-			'user_shipping_city'       => $user->get( 'shipping_city' ),
-			'user_shipping_postcode'   => $user->get( 'shipping_postcode' ),
-			'user_shipping_country'    => $user->get( 'shipping_country' ),
-			'user_shipping_state'      => $user->get( 'shipping_state' ),
-			'user_shipping_phone'      => $user->get( 'shipping_phone' ),
+			'user_link_facebook'       => get_user_meta( $user->ID, 'facebook', true ),
+			'user_link_instagram'      => get_user_meta( $user->ID, 'instagram', true ),
+			'user_link_linkedin'       => get_user_meta( $user->ID, 'linkedin', true ),
+			'user_link_myspace'        => get_user_meta( $user->ID, 'myspace', true ),
+			'user_link_pinterest'      => get_user_meta( $user->ID, 'pinterest', true ),
+			'user_link_youtube'        => get_user_meta( $user->ID, 'youtube', true ),
+			'user_link_twitter'        => get_user_meta( $user->ID, 'twitter', true ),
+			'user_link_tumblr'         => get_user_meta( $user->ID, 'tumblr', true ),
+			'user_link_soundcloud'     => get_user_meta( $user->ID, 'soundcloud', true ),
+			'user_link_wikipedia'      => get_user_meta( $user->ID, 'wikipedia', true ),
+			'user_billing_first_name'  => get_user_meta( $user->ID, 'billing_first_name', true ),
+			'user_billing_last_name'   => get_user_meta( $user->ID, 'billing_last_name', true ),
+			'user_billing_company'     => get_user_meta( $user->ID, 'billing_company', true ),
+			'user_billing_address_1'   => get_user_meta( $user->ID, 'billing_address_1', true ),
+			'user_billing_address_2'   => get_user_meta( $user->ID, 'billing_address_2', true ),
+			'user_billing_city'        => get_user_meta( $user->ID, 'billing_city', true ),
+			'user_billing_postcode'    => get_user_meta( $user->ID, 'billing_postcode', true ),
+			'user_billing_country'     => get_user_meta( $user->ID, 'billing_country', true ),
+			'user_billing_state'       => get_user_meta( $user->ID, 'billing_state', true ),
+			'user_billing_phone'       => get_user_meta( $user->ID, 'billing_phone', true ),
+			'user_billing_email'       => get_user_meta( $user->ID, 'billing_email', true ),
+			'user_shipping_first_name' => get_user_meta( $user->ID, 'shipping_first_name', true ),
+			'user_shipping_last_name'  => get_user_meta( $user->ID, 'shipping_last_name', true ),
+			'user_shipping_company'    => get_user_meta( $user->ID, 'shipping_company', true ),
+			'user_shipping_address_1'  => get_user_meta( $user->ID, 'shipping_address_1', true ),
+			'user_shipping_address_2'  => get_user_meta( $user->ID, 'shipping_address_2', true ),
+			'user_shipping_city'       => get_user_meta( $user->ID, 'shipping_city', true ),
+			'user_shipping_postcode'   => get_user_meta( $user->ID, 'shipping_postcode', true ),
+			'user_shipping_country'    => get_user_meta( $user->ID, 'shipping_country', true ),
+			'user_shipping_state'      => get_user_meta( $user->ID, 'shipping_state', true ),
+			'user_shipping_phone'      => get_user_meta( $user->ID, 'shipping_phone', true ),
 			'user_role_caps'           => Helper::array_to_string( $user->get_role_caps() ),
+			'user_role'                => empty( $user->roles ) ? '' : implode( '|', $user->roles ),
 		];
 	}
 
